0fe046b8ee3a5e7471ff1a6420e7e1ea0829d5eb
VikitorXD <rm551886@fiap.com.br>
2024-09-15 16:33:21 -0300

    cirando classe user

---
+++ medconnect/src/main/java/com/medconnect/model/usuario/User.java
@@ -0,0 +1,70 @@
+package com.medconnect.model.usuario;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="t_mdct_users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    @Column(name="id_users")
+    private Long idUser;
+
+    @Column(name="log_users")
+    private String login;
+
+    @Column(name="pass_users")
+    private String senha;
+
+    @Column(name="role_users")
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
