e3bb0ee6647685aa59c3b128aaf0c433185524f0
VikitorXD <rm551886@fiap.com.br>
2024-09-15 16:39:32 -0300

    adicionado o user repository e pacote service

--- medconnect/src/main/java/com/medconnect/model/usuario/User.java
+++ medconnect/src/main/java/com/medconnect/model/usuario/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -35,7 +36,8 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
---
+++ medconnect/src/main/java/com/medconnect/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.medconnect.repository;
+
+import com.medconnect.model.usuario.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
+
+}
