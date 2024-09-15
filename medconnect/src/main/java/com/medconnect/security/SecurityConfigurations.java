7ace1b137f9c59aadb03e2a23584bc1791f78812
VikitorXD <rm551886@fiap.com.br>
2024-09-15 19:29:35 -0300

    Atualização na permissão do token

--- medconnect/src/main/java/com/medconnect/security/SecurityConfigurations.java
+++ medconnect/src/main/java/com/medconnect/security/SecurityConfigurations.java
@@ -32,14 +32,6 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/clientes").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/clientes").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/clientes").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/clientes").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/produtos").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/produtos").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
